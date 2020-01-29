73370d06906cd1c23764341d672de386f59b8953
ShreyaDpatel <shreyapatel2305@gmail.com>
2020-01-28 20:50:56 -0800

    part1
    part 1 complete

---
+++ Shreya/script7.js
@@ -0,0 +1,61 @@
+function onEnterPressed(event){
+    if(event.key !== "Enter"){
+        return;
+    }
+
+    addEntry();
+}
+
+var id=0;
+
+var Entries =["Shreya","Krunal","Diana","Dhawal"];
+
+function addEntry() {
+
+
+    // snippet = `
+    // <tr id="${id}">
+    //     <td onClick="removeEntry(${id})"}>❌</td>
+    //     <td>${getName()}</td>
+    // </tr>
+    // `
+    // id +=1;
+    // setName("");
+
+    // document.getElementById("tbody").innerHTML += snippet;
+
+    var input=getName();
+
+    Entries.push(input);
+
+    localStorage.setItem('NameAry', JSON.stringify(Entries));
+
+    refresh();
+}
+
+function removeEntry(id) {
+    document.getElementById(id).remove();
+}
+
+function getName() {
+    return document.getElementById("name").value;
+}
+
+function setName(name) {
+    return document.getElementById("name").value=name;
+}
+
+function refresh()
+{
+    var html='';
+       for(var i=0; i<Entries.length; i++)
+       {
+                html+=`<tr id="${id}"><td onClick="removeEntry(${id})"}>❌</td><td>`+Entries[i]+'</td>'
+            
+                html +='</tr>'
+
+       }
+       // Check this is something strange happens
+           document.getElementById("tbody").innerHTML = html;
+
+}
